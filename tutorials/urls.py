3cfcb25153e2d0c44ca0a4b6e52f4d4c00a2049a
FranciscoBaltodano <valeadasfran30@gmail.com>
2023-04-20 22:08:06 -0600

    actualizacion

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -1,9 +1,7 @@
-from django.conf.urls import url 
+from django.conf.urls import re_path
 from tutorials import views 
 
 urlpatterns = [ 
-    url(r'^', include('tutorials.urls')),
-    url(r'^api/tutorials$', views.tutorial_list),
-    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
-    url(r'^api/tutorials/published$', views.tutorial_list_published)
+    re_path(r'^api/tutorials$', views.tutorial_detail),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorials_api),
 ]
--- tutorials/views.py
+++ tutorials/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
@@ -8,8 +8,6 @@ from tutorials.models import Tutorial
 from tutorials.serializers import TutorialSerializer
 from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST', 'DELETE'])
-def tutorial_list(request):
 
 @api_view(['GET','PUT','DELETE'])
 def tutorial_detail(request, pk):
@@ -19,6 +17,28 @@ def tutorial_detail(request, pk):
         return JsonResponse({'mesage': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['GET'])
-def tutorial_list_published(request):
-    # GET all published tutorials
+@csrf_exempt 
+def tutorials_api(request, id=0):
+    if request.method=='GET':
+        tutorials = Tutorial.objects.all()
+        tutorials_serializer =  TutorialSerializer(tutorials, many = True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+    elif request.method == 'POST':
+        tutorial_data = JSONParser().parse(request)
+        tutorials_serializer = TutorialSerializer(data = tutorial_data)
+        if tutorials_serializer.is_valid():
+            tutorials_serializer.save()
+            return JsonResponse('Añadido Exitosamente!!!',safe=False)
+        return JsonResponse("Fallo al intentar agregar", safe=False)
+    elif request.method=='PUT':
+        tutorial_data = JSONParser().parse(request)
+        tutorial = Tutorial.objects.get(id=tutorial_data['id'])
+        tutorials_serializer = TutorialSerializer(tutorial, data=tutorial_data)
+        if tutorials_serializer.is_valid():
+            tutorials_serializer.save()
+            return JsonResponse('Actualizado Exitosamente!!!!',safe=False)
+        return JsonResponse("Fallo al Actualizar")
+    elif request.method=='DELETE':
+        tutorial = Tutorial.objects.get(id=id)
+        tutorial.delete()
+        return JsonResponse('Eliminado Exitosamente', safe=False)
